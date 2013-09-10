ecde3eef03b9d617c86c8d677722109f2a38d5af
Mathieu Lajugie <mathieul@gmail.com>
2013-09-09 20:46:12 -0700

    add tests for binding and path info

--- test/cowboy/integration_test.exs
+++ test/cowboy/integration_test.exs
@@ -3,9 +3,35 @@ defmodule Cowboy.IntegrationTest do
 
   defmodule HttpHandler do
     @behaviour :cowboy_http_handler
-    def init({:tcp, :http}, req, _opts), do: {:ok, req, :undefined_state}
+    def init({ :tcp, :http }, req, _opts), do: { :ok, req, :undefined_state }
     def handle(req, state) do
       { :ok, req } = :cowboy_req.reply(200, [], "HttpHandler says hi /http", req)
+      { :ok, req, state }
+    end
+    def terminate(_reason, _req, _state), do: :ok
+  end
+
+  defmodule LoopHandler do
+    @behaviour :cowboy_loop_handler
+    def init({ :tcp, :http }, req, _opts) do
+      :timer.send_interval(150, { :return, "150ms have passed" })
+      { :loop, req, :undefined_state, 1000, :hibernate }
+    end
+    def info({ :return, message }, req, state) do
+      { :ok, req } = :cowboy_req.reply(200, [], message, req)
+      { :ok, req, state }
+    end
+    def terminate(_reason, _req, _state), do: :ok
+  end
+
+  defmodule InfoHandler do
+    @behaviour :cowboy_http_handler
+    def init({ :tcp, :http }, req, _opts), do: { :ok, req, :undefined_state }
+    def handle(req, state) do
+      { bindings, req } = :cowboy_req.bindings(req)
+      { path_info, req } = :cowboy_req.path_info(req)
+      body = "bindings=#{inspect bindings} path_info=#{inspect path_info}"
+      { :ok, req } = :cowboy_req.reply(200, [], body, req)
       { :ok, req, state }
     end
     def terminate(_reason, _req, _state), do: :ok
@@ -20,21 +46,37 @@ defmodule Cowboy.IntegrationTest do
   defmodule CowboyRoutes do
     use WebsocketWriter.Cowboy.Dispatch
     match "/http", to: HttpHandler
+    match "/loop", to: LoopHandler
+    match "/info/:first/[...]", to: InfoHandler
     default with: RouterApp
   end
 
-  test "basic request on a router app" do
-    start_server RouterApp, 8011, CowboyRoutes
+  setup_all do
+    Dynamo.Cowboy.run RouterApp, port: 8011, verbose: false, dispatch: CowboyRoutes.dispatch
+    :ok
+  end
+
+  teardown_all do
+    Dynamo.Cowboy.shutdown RouterApp
+    :ok
+  end
+
+  test "request routed to dynamo" do
     assert { 200, _, "RouterApp says wassup /" } = request :get, "/"
+  end
+
+  test "request routed to a HTTP handler" do
     assert { 200, _, "HttpHandler says hi /http" } = request :get, "/http"
-    stop_server RouterApp
   end
 
-  defp start_server(app, port, routes) do
-    Dynamo.Cowboy.run(app, port: port, verbose: false, dispatch: routes.dispatch)
+  test "request routed to a Loop handler" do
+    assert { 200, _, "150ms have passed" } = request :get, "/loop"
   end
 
-  defp stop_server(app), do: Dynamo.Cowboy.shutdown(app)
+  test "request returning its bindings and path info" do
+    { 200, _, body } = request :get, "/info/allo/la/terre"
+    assert body == %s(bindings=[first: "allo"] path_info=["la", "terre"])
+  end
 
   defp request(verb, path) do
     { :ok, status, headers, client } =
